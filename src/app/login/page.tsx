46e38d89abac533335c7f053dbcc6d2647d0d217
jiayishen21 <lucasshen21@gmail.com>
2025-05-24 01:59:53 -0700

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,39 @@
+"use client"
+
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Login() {
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleLogin = () => {
+		fetch('/api/auth/login', {
+			method: 'POST',
+			body: JSON.stringify({ email, password }),
+		})
+			.then(res => res.json())
+			.then(data => {
+				console.log(data)
+			})
+			.catch(err => {
+				console.error(err)
+			})
+	}
+
 	return (
 		<div>
-			Login
+			<div>
+				<Link href="/">Back to home</Link>
+			</div>
+			<div>
+				Login
+			</div>
+			<div>
+				<input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+				<input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+				<button onClick={handleLogin}>Login</button>
+			</div>
 		</div>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
 	return (
 		<div>
-			Home
+			<div>
+				Home
+			</div>
+			<div>
+				<Link href="/login">Login</Link>
+			</div>
+			<div>
+				<Link href="/signup">Signup</Link>
+			</div>
 		</div>
 	);
 }

82c50ecba2b04c96d75388d597f9004e00417a4f
jiayishen21 <lucasshen21@gmail.com>
2025-05-25 01:19:40 -0700

    clear token tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 "use client"
 
 import Link from "next/link";
+import { LOCALSTORAGE_JWT_KEY } from "./constants/auth";
+import { ClientLogger } from "@/utils/clientLogger";
 
 export default function Home() {
+	const handleClearToken = () => {
+		localStorage.removeItem(LOCALSTORAGE_JWT_KEY)
+		ClientLogger.info("Cleared local token")
+	}
+
 	return (
 		<div>
 			<div>
@@ -14,6 +21,11 @@ export default function Home() {
 			<div>
 				<Link href="/signup">Signup</Link>
 			</div>
+			<button
+				onClick={handleClearToken}
+			>
+				Clear local token
+			</button>
 		</div>
 	);
 }

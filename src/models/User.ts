d6d99c321e7dcb4d72306f1ada6d671e7a22f4ac
jiayishen21 <lucasshen21@gmail.com>
2025-05-25 00:32:11 -0700

    date stuff

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,11 +3,23 @@ import mongoose, { Schema, Document } from 'mongoose'
 export interface IUser extends Document {
 	email: string
 	password: string
+	createdAt: number
 }
 
 const UserSchema = new Schema<IUser>({
-	email: { type: String, required: true },
-	password: { type: String, required: true },
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Number,
+		required: true,
+		default: () => Date.now(),
+	}
 })
 
 export const UserModel = mongoose.models.User || mongoose.model<IUser>('User', UserSchema)

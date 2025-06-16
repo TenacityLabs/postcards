a1ee539405780054546d62c68658123473b0564e
jiayishen21 <lucasshen21@gmail.com>
2025-06-15 18:06:24 -0700

    opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,19 @@ import { ModalProvider } from "./context/modalContext";
 export const metadata: Metadata = {
 	title: "Postcards",
 	description: "Write postcards to your friends and family to stay in touch.",
+	openGraph: {
+		title: "Postcards",
+		description: "Write postcards to your friends and family to stay in touch.",
+		images: [
+			{
+				url: "/images/metadata/postcards-og.jpg",
+				width: 1200,
+				height: 630,
+				alt: "Postcards - Send love, one update at a time.",
+			},
+		],
+		type: "website",
+	},
 };
 
 export default function RootLayout({
